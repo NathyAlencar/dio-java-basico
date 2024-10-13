ad8553ab83556652e643d916448bdaf77ce663b4
NathyAlencar <136849458+NathyAlencar@users.noreply.github.com>
2024-10-13 17:54:39 -0400

    create

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,53 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+
+        String plano = "M"; // M / T
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("Whats e Intagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+    }
+
+    //     String sigla = "M";
+    //     switch (sigla) {
+    //         case "P":{
+    //             System.out.println("PEQUENO");
+    //             break;
+    //         }
+    //         case "M":{
+    //             System.out.println("MEDIO");
+    //             break;
+    //         }
+    //         case "G":{
+    //             System.out.println("GRANDE");
+    //             break;
+    //         }
+    //         default:
+    //             System.out.println("INDEFINIDO");
+    //         }       
+    // }
+
+
+    //     // Modo condicional if/else
+	// 	String sigla = "M";
+
+	// 	if(sigla == "P")
+	// 		System.out.println("PEQUENO");
+	// 	else if(sigla == "M")
+	// 		System.out.println("MEDIO");
+	// 	else if(sigla == "G")
+	// 		System.out.println("GRANDE");
+	// 	else
+	// 		System.out.println("INDEFINIDO");
+			
+		
+	// }
+}
