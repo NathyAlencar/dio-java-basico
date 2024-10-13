f693128b03854c1a477471b4e83b2e864edd3ea8
NathyAlencar <136849458+NathyAlencar@users.noreply.github.com>
2024-10-13 18:36:05 -0400

    create

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,16 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos[] = {"FELIPE","JONAS","JULIA","MARCOS"};
+
+         for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+	    }
+
+
+        // for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++ ){
+        //     System.out.println("contando carneirinhos" + carneirinhos);
+        // }
+
+        // System.out.println("Joãozinho dormiu");
+    }
+}
